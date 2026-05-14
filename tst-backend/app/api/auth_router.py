c14d424bca0f7be48ed9d3d04f6baf30761dd9ac
joaoneto9 <joaonetocg1@gmail.com>
2026-05-14 00:00:51 -0300

    feat: create the function to check if the token is valid and if the email was verified

--- tst-backend/app/api/auth_router.py
+++ tst-backend/app/api/auth_router.py
@@ -1,8 +1,8 @@
 import os
-from sys import exception
-from firebase_admin import credentials, initialize_app
-from fastapi import APIRouter
-from schemas.AuthSchemas import userSingUpRequest
+import firebase_admin
+from firebase_admin import credentials, auth
+from fastapi import APIRouter, HTTPException, Header
+from schemas.AuthSchemas import UserSingUpRequest
 
 router = APIRouter(
     prefix="/auth",
@@ -16,8 +16,31 @@ if not firebase_credential_path:
     raise ValueError('Credenciais nao encontradas no path.')
 
 cred = credentials.Certificate(firebase_credential_path)
-default_app = initialize_app(cred)
+default_app = firebase_admin.initialize_app(cred)
 
-@router.post("/")
-def singup():
-    return "usuario realizou login com sucesso."
+# criando uma funcao de dependencia para a rota de singup
+# antes de realizar a rota que for selecionada como dependent, ela executara isso primeiro.
+async def verify_user(id_token: str = Header(None)):
+    if not id_token or not id_token.startswith("Bearer "):
+        raise HTTPException(
+            status_code=401,
+            detail="Token ausente."
+        )
+    
+    token = id_token.split("Bearer ")[1]
+
+    try:
+        decoded_token = auth.verify_id_token(token)
+
+        if not decoded_token.get("email_verified"):
+            raise HTTPException(
+                status_code=403,
+                detail="Email não validado pelo usuário."
+            )
+        
+        return decoded_token
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Token Invalido."
+        )
