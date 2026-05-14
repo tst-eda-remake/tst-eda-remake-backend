c7f71672249014b8cf583c98e2f0b398f730e07a
joaoneto9 <joaonetocg1@gmail.com>
2026-05-14 00:59:16 -0300

    feat: persisting the just router functions and documenting the signup enpoint

--- tst-backend/app/api/auth_router.py
+++ tst-backend/app/api/auth_router.py
@@ -1,7 +1,5 @@
-import os
-import firebase_admin
-from firebase_admin import credentials, auth
-from fastapi import APIRouter, Depends, HTTPException, Header
+from services.auth_services import verify_user
+from fastapi import status, APIRouter, Depends
 from schemas.AuthSchemas import UserSignupRequest, SigninResponse
 
 router = APIRouter(
@@ -9,47 +7,36 @@ router = APIRouter(
     tags=["authentication"]
 )
 
-firebase_credential_path = os.getenv('PATH_FIREBASE_CREDENTIALS')
-
-if not firebase_credential_path:
-    print("erro ao procurar as credencias do Firebase.")
-    raise ValueError('Credenciais nao encontradas no path.')
-
-cred = credentials.Certificate(firebase_credential_path)
-default_app = firebase_admin.initialize_app(cred)
-
-# criando uma funcao de dependencia para a rota de singup
-# antes de realizar a rota que for selecionada como dependent, ela executara isso primeiro.
-async def verify_user(id_token: str = Header(None)):
-    if not id_token or not id_token.startswith("Bearer "):
-        raise HTTPException(
-            status_code=401,
-            detail="Token ausente."
-        )
-    
-    token = id_token.split("Bearer ")[1]
-
-    try:
-        decoded_token = auth.verify_id_token(token)
-
-        if not decoded_token.get("email_verified"):
-            raise HTTPException(
-                status_code=403,
-                detail="Email não validado pelo usuário."
-            )
-        
-        return decoded_token
-    except Exception:
-        raise HTTPException(
-            status_code=401,
-            detail="Token Invalido."
-        )
-
-@router.post("/singup")
+@router.post(
+    "/signup",
+    response_model=SigninResponse,
+    status_code=status.HTTP_201_CREATED,
+    summary="Cadastra um novo usuário",
+    description="Cria o registro do usuário no banco de dados interno após a validação do token Firebase."
+)
 async def signup(userSignup: UserSignupRequest, decoded_token = Depends(verify_user)):
+    """
+    Realiza o cadastro do usuário no sistema.
+
+    Args:
+        user_signup (UserSignupRequest): Dados do perfil do usuário (nome, cargo, etc).
+        decoded_token (dict): Token JWT decodificado, injetado pela dependência de verificação.
+
+    Returns:
+        SigninResponse: Objeto contendo os dados do usuário cadastrado com sucesso.
+
+    Raises:
+        HTTPException (401): Caso o header esteja ausente, malformado ou o token seja inválido.
+        HTTPException (403): Caso o token seja válido, mas o e-mail não tenha sido verificado.
+        HTTPExecption (409): Se o usuário ja apresentar cadastro.
+    """
     # antes de rodar o escopo da função, o Depends é chamado e aguada-se a autorização
+
+    # aqui chamaria a logica interna no services para o cadastro do aluno no DB
+
+    # Retorno caso não suba escessão.
     return {
-        "status": 200,
+        "status": 201,
         "user_info": SigninResponse(
             first_name=userSignup.first_name,
             last_name=userSignup.last_name,
@@ -57,10 +44,4 @@ async def signup(userSignup: UserSignupRequest, decoded_token = Depends(verify_u
             role=userSignup.role,
             uid=decoded_token.get("uid")
         )
-    }
-
-@router.get("/")
-def hello_world():
-    return {
-        "message": "hello, world"
     }
