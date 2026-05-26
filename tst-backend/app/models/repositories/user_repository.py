66a96c3bcab46cfb5d4dbea582669e576751c16a
joaoneto9 <joaonetocg1@gmail.com>
2026-05-26 14:43:51 -0300

    feat: defining the __init__ contructor with a db session as a parahmeter

--- tst-backend/app/models/repositories/user_repository.py
+++ tst-backend/app/models/repositories/user_repository.py
@@ -8,8 +8,8 @@ from models.user_model import User
 
 class UserRepository:
     
-    def __init__(self):
-        self.db: Session = session_local()
+    def __init__(self, db: Optional[Session] = None):
+        self.db: Session = db if db is not None else session_local()
 
     def get_user_by_uid(self, uid: str) -> Optional[User]:
         return self.db.query(
