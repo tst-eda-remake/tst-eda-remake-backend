5e75bd607e8aa6319098a9eafaeb17c6a7e69f31
Guilherme <guilhermenoronhaf@gmail.com>
2026-05-30 12:36:34 -0300

    feat: create QuestioUpdate schema

--- tst-backend/app/schemas/question_schema.py
+++ tst-backend/app/schemas/question_schema.py
@@ -8,6 +8,14 @@ class QuestionCreate(BaseModel):
     output_format: str
     resolution_path: str
 
+class QuestionUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    restriction: str | None = None
+    input_format: str | None = None
+    output_format: str | None = None
+    resolution_path: str | None = None
+
 class QuestionResponse(BaseModel):
     id: int
     title: str
