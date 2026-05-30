ee85daa889b49687309847fc01850f66f99d2ff3
Guilherme <guilhermenoronhaf@gmail.com>
2026-05-30 10:19:45 -0300

    feat: create a repository for questions

---
+++ tst-backend/app/models/repositories/question_repository.py
@@ -0,0 +1,55 @@
+from typing import Optional, final
+
+from sqlalchemy import delete
+from sqlalchemy.exc import IntegrityError
+
+from app.models.config.database_config import session_local
+from sqlalchemy.orm import Session
+from app.models.question_model import Question
+
+class QuestionRepository:
+
+    def __init__(self, db: Optional[Session] = None):
+        self.db = db if db is not None else session_local()
+
+    def get_question_by_id(self, id: int) -> Optional[Question]:
+        return self.db.query(
+                Question
+        ).filter(Question.id == id).first()
+
+    def insert_question(self, question: Question):
+        try:
+            self.db.add(question)
+
+            self.db.commit()
+            return True
+        except IntegrityError:
+            self.db.rollback()
+            return False
+        finally:
+            self.db.close()
+
+    def save_changes(self):
+        try:
+            self.db.commit()
+            return True
+        except:
+            self.db.rollback()
+            return False
+        finally:
+            self.db.close()
+
+    def delete_question(self, id: int):
+        try:
+            lines = self.db.query(Question).filter(Question.id == id).delete(
+                    synchronize_session="evaluate"
+            )
+
+            self.db.commit()
+
+            return lines > 0
+        except Exception:
+            self.db.rollback()
+            return False
+        finally:
+            self.db.close()
