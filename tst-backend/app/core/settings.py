1af0a8df26b150f5e644e9d6c88a5a5f56b04163
Guilherme <guilhermenoronhaf@gmail.com>
2026-06-01 22:13:06 -0300

    raw: modifying app/core/settings.py so it has default values for database_url and path_firebase_credentials. On production, gets from .env

--- tst-backend/app/core/settings.py
+++ tst-backend/app/core/settings.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    PATH_FIREBASE_CREDENTIALS: str  # O Pydantic mapeia direto o nome correto do .env
+    DATABASE_URL: str = "sqlite:///:memory:"
+    PATH_FIREBASE_CREDENTIALS: str | None = None # O Pydantic mapeia direto o nome correto do .env
 
     model_config = SettingsConfigDict(
         env_file=".env",
