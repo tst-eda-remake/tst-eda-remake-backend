a1875cef9bc3fcd513f0b653b6f3b8476bd1c0b4
joaoneto9 <joaonetocg1@gmail.com>
2026-06-03 18:02:11 -0300

    feat: adding the global exception handler and seting it up when the app has been initialized

--- tst-backend/app/exceptions/global_exception_handler.py
+++ tst-backend/app/exceptions/global_exception_handler.py
@@ -2,29 +2,29 @@ from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from app.exceptions.question_exceptions import QuestionNotFoundException
 from app.exceptions.user_exceptions import UserNotFoundException
-from app.main import app
 
-@app.exception_handler(QuestionNotFoundException)
-async def question_not_found_handler(request: Request, exc: QuestionNotFoundException):
+def register_exception_handlers(app: FastAPI):
 
-    return JSONResponse(
-        status_code=status.HTTP_404_NOT_FOUND,
-        content={
-            "title": "Invalid Request",
-            "status": status.HTTP_404_NOT_FOUND,
-            "detail": exc.message,
-            "instance": request.url.path
-        }
-    )
-
-@app.exception_handler(UserNotFoundException)
-async def user_not_found_handler(request: Request, exc: UserNotFoundException):
-    return JSONResponse(
-        status_code=status.HTTP_404_NOT_FOUND,
-        content={
-            "title": "Invalid Request",
-            "status": status.HTTP_404_NOT_FOUND,
-            "detail": exc.message,
-            "instance": request.url.path
-        }
-    )
+    @app.exception_handler(QuestionNotFoundException)
+    async def question_not_found_handler(request: Request, exc: QuestionNotFoundException):
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "title": "Invalid Request",
+                "status": status.HTTP_404_NOT_FOUND,
+                "detail": exc.message,
+                "instance": request.url.path
+            }
+        )
+    
+    @app.exception_handler(UserNotFoundException)
+    async def user_not_found_handler(request: Request, exc: UserNotFoundException):
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "title": "Invalid Request",
+                "status": status.HTTP_404_NOT_FOUND,
+                "detail": exc.message,
+                "instance": request.url.path
+            }
+        )
--- tst-backend/app/main.py
+++ tst-backend/app/main.py
@@ -4,6 +4,7 @@ from app.api import user_router
 from app.api import question_router
 from contextlib import asynccontextmanager
 from app.models.config.database_config import init_db 
+from app.exceptions.global_exception_handler import register_exception_handlers
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,15 +15,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# adicionando as rotas do diretorio api
+register_exception_handlers(app)
+
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
 app.include_router(question_router.router)
-
-@app.get("/")
-def hello_world():
-    print("hello world")
-    return {
-        "status": 200,
-        "response": "ola mundo"
-    }
