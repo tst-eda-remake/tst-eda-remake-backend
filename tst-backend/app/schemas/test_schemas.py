8909be7e0ae1883b57ce228021fa28944e274856
joaoneto9 <joaonetocg1@gmail.com>
2026-06-09 10:40:34 -0300

    feat: adding the test schemas for creation and response

---
+++ tst-backend/app/schemas/test_schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class TestResponse(BaseModel):
+    id: int
+    input: str
+    output: str
+    is_public: bool
+
+class TestCreate(BaseModel):
+    input: str
+    output: str
+    is_public: bool
+    question_id: int
