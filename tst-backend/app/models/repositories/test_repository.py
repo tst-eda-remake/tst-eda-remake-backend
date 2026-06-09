e783eecca06fabbb87b5e6cd31dac1fa13d8c1fd
joaoneto9 <joaonetocg1@gmail.com>
2026-06-09 14:59:39 -0300

    feat: adding the delete function

--- tst-backend/app/models/repositories/test_repository.py
+++ tst-backend/app/models/repositories/test_repository.py
@@ -1,6 +1,8 @@
 from typing import Optional
+from httpx import delete
 from psycopg import IntegrityError
 from sqlalchemy.orm import Session
+from app.exceptions.test_exception_handler import TestNotFoundException
 from app.models.config.database_config import session_local
 from app.models.test_model import Test
 
@@ -8,6 +10,17 @@ class TestRepository:
 
     def __init__(self, db: Optional[Session] = None):
         self.db = db if db is not None else session_local()
+
+    def delete(self, test_id: int):
+        try:
+            lines = self.db.query(Test).filter(Test.id == test_id).delete(
+                synchronize_session=False   
+            )
+
+            self.db.commit()
+            return lines > 0
+        except:
+            return False
 
     def save(self, test: Test):
         try:
